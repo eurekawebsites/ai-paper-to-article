6da2b8f084bb112be51b32c8a0d4b52c01bd9fa8
eurekawebsites <121565788+eurekawebsites@users.noreply.github.com>
2023-01-16 20:23:51 -0500

    changed prompt &max token

--- ai_paper_to_article_gpt_3_2023_01_16_av_v4.py
+++ ai_paper_to_article_gpt_3_2023_01_16_av_v4.py
@@ -9,14 +9,14 @@ openai.api_key = OPENAI_API_KEY
 name_of_article = st.text_input("Nombre del artículo científico: ")
 
 # Define the prompt for the model
-prompt = (f"Actúa como periodista y escribe un artículo en español para el público en general. Explica los resultados y proporciona números para respaldar tus afirmaciones. Acerca del artículo titulado {name_of_article}. Lang: es")
+prompt = (f"act as a scientific journalist and write an article in Spanish for the general public. Explain the results and provide numbers to back up their claims. About the scientific paper {name_of_article}. Lang: es")
 
 # Generate text using the GPT-3 model
 response = openai.Completion.create(
     engine="text-davinci-003",
     prompt=prompt,
     temperature=0.3,
-    max_tokens=4000,
+    max_tokens=4030,
     n = 1,
     stop=None,
     top_p = 1,
