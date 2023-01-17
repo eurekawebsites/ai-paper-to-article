81bf75e85cbf9e5c67b455166cb0969f65b8739a
eurekawebsites <121565788+eurekawebsites@users.noreply.github.com>
2023-01-17 08:12:12 -0500

    Update ai_paper_to_article_gpt_3_2023_01_16_av_v4.py

--- ai_paper_to_article_gpt_3_2023_01_16_av_v4.py
+++ ai_paper_to_article_gpt_3_2023_01_16_av_v4.py
@@ -7,11 +7,12 @@ st.write("**✨ AI GENERATED ARTICLE FROM PUBLISHED PAPER ✨**")
 OPENAI_API_KEY = st.text_input("Insert your OpenAI API key here:")
 openai.api_key = OPENAI_API_KEY
 
-# Get the name of the article from the user
+# Get the name of the article, and first author, from the user
 name_of_article = st.text_input("Nombre del artículo científico: ")
+name_of_author = st.text_input("Nombre del primer autor: ")
 
 # Define the prompt for the model
-prompt = (f"act as a scientific journalist and write an article in Spanish for the general public. Explain the results and provide numbers to back up their claims. About the scientific paper {name_of_article}. Lang: es")
+prompt = (f"act as a scientific journalist and write an article in Spanish for the general public. Explain the results and provide numbers to back up their claims. About the scientific paper {name_of_article} by {name_of_author}. Lang: es")
 
 # Generate text using the GPT-3 model
 response = openai.Completion.create(
