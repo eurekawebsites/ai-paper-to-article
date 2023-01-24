f0c397803e6e1bda792a298e27fe0cbff39a267a
eurekawebsites <121565788+eurekawebsites@users.noreply.github.com>
2023-01-24 09:59:36 -0500

    Update ai_paper_to_article_gpt_3_2023_01_16_av_v4.py

--- ai_paper_to_article_gpt_3_2023_01_16_av_v4.py
+++ ai_paper_to_article_gpt_3_2023_01_16_av_v4.py
@@ -7,28 +7,24 @@ st.write("**✨ AI GENERATED ARTICLE FROM PUBLISHED PAPER ✨**")
 OPENAI_API_KEY = st.text_input("Insert your OpenAI API key here:")
 openai.api_key = OPENAI_API_KEY
 
-# Get the info of the paper from the user
-doi = st.text_input("DOI: ")
-name_paper = st.text_input("Name of paper: ")
-
 # Add a dropdown menu to select the language
 languages = ["English", "Español", "Français", "Deutsch", "Italiano","Nederlands"]
 language = st.selectbox("Select the language for the output:", languages)
 
+# Get the info of the paper from the user
+name_paper = st.text_input("Name of paper: ")
+
 # Define the prompt for the model 
-prompt = (f"Summarize the results of the research paper {name_paper}, with DOI {doi}, and give the numbers that backup claims. Lang: {language}")
+prompt = (f"I am a journalist. Help me by pointing out the most important aspects of this paper: {name_paper}. Lang: {language}")
 
 # Generate text using the GPT-3 model
 response = openai.Completion.create(
     engine="text-davinci-003",
     prompt=prompt,
     temperature=0.5,
-    max_tokens= 2000,
+    max_tokens= 4000,
     n = 1,
     stop=None,
-    top_p = 1,
-    frequency_penalty = 0,
-    presence_penalty = 0,
 )
 
 # Print the generated text
